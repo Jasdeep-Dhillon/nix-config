45ddedd2d19a003c1d6c3a1f867cb82e89dea09f
Jasdeep <jsdhillon286@gmail.com>
2026-04-06 03:28:00 -0400

    define base modules
    Change-Id: If5c52170622ec4675e23a53be84060fa6a6a6964

---
+++ base/settings.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  flake.nixosModules.settings = {
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+    
+    nixpkgs.config.allowUnfree = true;
+  };
+}

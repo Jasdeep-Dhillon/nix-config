8c81ce1c3fb2a5af4a6c2e91776e71eee08a69b5
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-04-30 02:49:24 -0400

    system/network-share: add smb mounts to legion

--- system/network-share.nix
+++ system/network-share.nix
@@ -49,4 +49,28 @@
       options = [ "bind" ];
     };
   };
+
+  flake.nixosModules.legion =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [ pkgs.cifs-utils ];
+      fileSystems."/media/lain/Storage" = {
+        device = "//lain/storage";
+        fsType = "cifs";
+        options =
+          let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+          in
+          [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+      };
+      fileSystems."/media/lain/External" = {
+        device = "//lain/external";
+        fsType = "cifs";
+        options =
+          let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+          in
+          [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+      };
+    };
 }

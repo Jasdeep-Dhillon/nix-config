88d8fd8763d826d389ff9a44f404ad8bb6635ecc
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-05-04 17:35:57 -0400

    wrapped/fuzzel: change styling

--- wrapped/fuzzel.nix
+++ wrapped/fuzzel.nix
@@ -13,23 +13,32 @@
             show-actions = true;
             launch-prefix = lib.getExe pkgs.runapp;
             width = 60;
+            # prompt = "'Launch: '";
+            placeholder = "Search";
             horizontal-pad = 60;
             vertical-pad = 32;
-            keyboard-focus = "exclusive";
+            inner-pad = 12;
+            line-height = 32;
+            keyboard-focus = "on-demand";
           };
           colors = {
-            background="1e1e2edd";
-            text="cdd6f4ff";
-            prompt="bac2deff";
-            placeholder="7f849cff";
-            input="cdd6f4ff";
-            match="b4befeff";
-            selection="585b70ff";
-            selection-text="cdd6f4ff";
-            selection-match="b4befeff";
-            counter="7f849cff";
-            border="b4befeff";
+            background = "1e1e2edd";
+            text = "cdd6f4ff";
+            prompt = "bac2deff";
+            placeholder = "7f849cff";
+            input = "cdd6f4ff";
+            match = "b4befeff";
+            selection = "585b70ff";
+            selection-text = "cdd6f4ff";
+            selection-match = "b4befeff";
+            counter = "7f849cff";
+            border = "b4befeff";
           };
+          border = {
+            width = 2;
+            selection-radius = 8;
+          };
+
         };
       };
     };

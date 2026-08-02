a62bd2012988225b419bc924189f1ff632bc1e79
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-08-02 04:45:41 -0400

    modules/tailscale: restart tailscale after sleep and connect/disconnect

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,7 +1,12 @@
 { ... }:
 {
   flake.nixosModules.tailscale =
-    { config, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
     {
 
       services.tailscale = {
@@ -16,6 +21,42 @@
       };
 
       systemd.services.tailscaled.serviceConfig.Environment = [ "TS_DEBUG_FIREWALL_MODE=nftables" ];
+
+      systemd.services.tailscale-down = {
+        description = "Tailscale down before suspend";
+        before = [ "sleep.target" ];
+        wants = [ "sleep.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${lib.getExe config.services.tailscale.package} down";
+        };
+      };
+
+      systemd.services.tailscale-up = {
+        description = "Tailscale up after resume";
+        after = [
+          "resume.target"
+          "tailscale-down.service"
+        ];
+        wants = [ "resume.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${lib.getExe config.services.tailscale.package} up";
+        };
+      };
+
+      networking.networkmanager.dispatcherScripts = [
+        {
+          source = pkgs.writeShellScript "tailscale-restart-on-netchange" ''
+            case "$2" in
+              up|down)
+                ${lib.getExe config.services.tailscale.package} down
+                ${lib.getExe config.services.tailscale.package} up
+                ;;
+            esac
+          '';
+        }
+      ];
 
       systemd.network.wait-online.enable = false;
       boot.initrd.systemd.network.wait-online.enable = false;

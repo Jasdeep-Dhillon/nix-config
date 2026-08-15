3695a580a07b8843ba34fb8a06a890169cca89cc
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-08-15 14:33:55 -0400

    flake: update lanzaboote to latest release tag

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
     home-manager.url = "github:nix-community/home-manager";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/001e560fffc8f0235e9db20ebeb4ccde0ade1caf";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     run0-sudo-shim = {

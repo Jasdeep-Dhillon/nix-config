0698e5aa62df3f2416ecf65679bec64302096f8f
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-09-03 01:54:55 -0400

    modules/browser: use helium-widevine-beta, update helium flake

--- modules/browser.nix
+++ modules/browser.nix
@@ -1,7 +1,7 @@
 { inputs, ... }: {
   flake.homeModules.browser = { pkgs, ... }: {
     home.packages = [
-      inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.helium-widevine
+      inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.helium-widevine-beta
       inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
   };
